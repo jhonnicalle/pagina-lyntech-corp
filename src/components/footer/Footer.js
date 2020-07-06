c5440ce8b1964d6a179292689dcc8e76f8f01dfe
Jhonni Calle <jhonnicalle@gmail.com>
2020-07-06 15:52:55 -0400

    Adding fixes design

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,23 +4,21 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Fade from 'react-reveal/Fade';
-import Flip from 'react-reveal/Flip';
-
 
 class Footer extends Component {
   render() {
     return(
-      <Fade>
+      <Fade buttom cascade>
         <footer className="site-footer">
-          <h2>Contactanos</h2>
+          <h2>Contáctanos</h2>
           <Container>
             <Row xs={1} md={1} lg={2}>
               <Col>
-                <h6>Direccion</h6>
+                <h6>Dirección</h6>
                 <p>Calle 2 Sur #33-29. Medellín. Colombia.</p>
               </Col>
               <Col>
-                <h6>Correo Electronico</h6>
+                <h6>Correo Electrónico</h6>
                 <p>gerencia@lyntechcorp.com</p>
               </Col>
             </Row>
--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -4,13 +4,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './services.css'
 
-
+import Fade from 'react-reveal/Fade'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+
 
 class Services extends Component {
   render() {
+    //This const is to set the values settings of the carousel
     const settings = {
       dots: true,
       // infinite: true,
@@ -47,59 +47,69 @@ class Services extends Component {
       ]
     };
     return(
-      <div className='service'>
-        <Container>
-          <Slider {...settings}>
-            <div className='card'>
-              <h3>Servicios Maritimos</h3>
-              <ul>
-                <li>Modelos Estratégicos de Gestión</li>
-                <li>Optimización de Flota y Operaciones</li>
-                <li>Desarrollo de nuevos negocios</li>
-                <li>Planes de negocios</li>
-                <li>Operaciones corporativas</li>
-                <li>Concursos y Ofertas</li>
-                <li>Financiación de activos</li>
-                <li>Reestructuración de flotas</li>
-              </ul>
-            </div>
-            <div className='card'>
-              <h3>Servicios Portuarios</h3>
-              <ul>
-                <li>Licitación de nuevas terminales</li>
-                <li>Planificación de equipos y superficies</li>
-                <li>Reordenación de espacios</li>
-                <li>Operaciones corporativas</li>
-                <li>Concursos y Ofertas</li>
-                <li>Servicios Portuarios</li>
-                <li>Operaciones de financiación</li>
-                <li>Planes de negocios</li>
-              </ul>
-            </div>
-            <div className='card'>
-              <h3>Servicios Intermodales</h3>
-              <ul>
-                <li>Elaboración de proyectos</li>
-                <li>Operativa cadenas intermodales -SSS</li>
-                <li>Desarrollo de autopistas del mar</li>
-                <li>Proyectos para grandes cargadores</li>
-                <li>Participación en proyectos TEN-T</li>
-                <li>Valoraciones económicas</li>
-              </ul>
-            </div>
-            <div className='card'>
-              <h3>Para el conjunto del sector</h3>
-              <ul>
-                <li>Elaboración de Planes estratégicos</li>
-                <li>Restructuraciones y optimización de costes</li>
-                <li>Adaptación de ERPs a las necesidades del sector</li>
-                <li>Modelización de herramientas de gestión</li>
-                <li>Estudios y optimización de procesos</li>
-              </ul>
-            </div>
-          </Slider>
-        </Container>
-      </div>
+      <Fade bottom cascade>
+        <div className='service'>
+          <Container>
+            <Slider {...settings}>
+              <div className='card-1'>
+                <div className='card'>
+                  <h3>Servicios Marítimos</h3>
+                  <ul>
+                    <li>Modelos Estratégicos de Gestión</li>
+                    <li>Optimización de Flota y Operaciones</li>
+                    <li>Desarrollo de Nuevos Negocios</li>
+                    <li>Planes de Negocios</li>
+                    <li>Operaciones Corporativas</li>
+                    <li>Concursos y Ofertas</li>
+                    <li>Financiación de Activos</li>
+                    <li>Reestructuración de Flotas</li>
+                  </ul>
+                </div>
+              </div>
+              <div className='card-2'>
+                <div className='card'>
+                  <h3>Servicios Portuarios</h3>
+                  <ul>
+                    <li>Licitación de Nuevas Terminales</li>
+                    <li>Planificación de Equipos y Superficies</li>
+                    <li>Reordenación de Espacios</li>
+                    <li>Operaciones Corporativas</li>
+                    <li>Concursos y Ofertas</li>
+                    <li>Servicios Portuarios</li>
+                    <li>Operaciones de Financiación</li>
+                    <li>Planes de Negocios</li>
+                  </ul>
+                </div>
+              </div>
+              <div className='card-3'>
+                <div className='card'>
+                  <h3>Servicios Intermodales</h3>
+                  <ul>
+                    <li>Elaboración de proyectos</li>
+                    <li>Operativa cadenas intermodales -SSS</li>
+                    <li>Desarrollo de autopistas del mar</li>
+                    <li>Proyectos para grandes cargadores</li>
+                    <li>Participación en Proyectos TEN-T</li>
+                    <li>Valoraciones Económicas</li>
+                  </ul>
+                </div>
+              </div>
+              <div className='card-4'>
+                <div className='card'>
+                  <h3>Para el Conjunto del Sector</h3>
+                  <ul>
+                    <li>Elaboración de Planes estratégicos</li>
+                    <li>Restructuraciones y optimización de costes</li>
+                    <li>Adaptación de ERPs a las necesidades del sector</li>
+                    <li>Modelización de Herramientas de Gestión</li>
+                    <li>Estudios y Optimización de Procesos</li>
+                  </ul>
+                </div>
+              </div>
+            </Slider>
+          </Container>
+        </div>
+      </Fade>
     )
   }
 }
