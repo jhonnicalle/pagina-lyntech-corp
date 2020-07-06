8e803b0996033a728ac55d61911c0341170aa2b0
Jhonni Calle <jhonnicalle@gmail.com>
2020-07-06 11:57:43 -0400

    Updates

---
+++ precache-manifest.4f4cbf32ec123dea4776f7c4402a2fdd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7264631a94ed4742f09c0329c81970",
+    "url": "/pagina-lyntech-corp/index.html"
+  },
+  {
+    "revision": "f51a43decefffe6e43d4",
+    "url": "/pagina-lyntech-corp/static/css/2.87451321.chunk.css"
+  },
+  {
+    "revision": "4439026e19305054db2c",
+    "url": "/pagina-lyntech-corp/static/css/main.a2bc4e0b.chunk.css"
+  },
+  {
+    "revision": "f51a43decefffe6e43d4",
+    "url": "/pagina-lyntech-corp/static/js/2.7ad9a116.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pagina-lyntech-corp/static/js/2.7ad9a116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4439026e19305054db2c",
+    "url": "/pagina-lyntech-corp/static/js/main.20e036ba.chunk.js"
+  },
+  {
+    "revision": "33ad765f620f53d86a1b",
+    "url": "/pagina-lyntech-corp/static/js/runtime-main.aa973cb1.js"
+  },
+  {
+    "revision": "b565c2367426e5917b624d3e3f718b2b",
+    "url": "/pagina-lyntech-corp/static/media/SERVICIOS.b565c236.jpg"
+  },
+  {
+    "revision": "761a23e59b0b180125004df8acc86516",
+    "url": "/pagina-lyntech-corp/static/media/diamante.761a23e5.svg"
+  },
+  {
+    "revision": "33efa4c0614a5c3efab6b2460e45a2b2",
+    "url": "/pagina-lyntech-corp/static/media/lyntech_logo-01.33efa4c0.png"
+  },
+  {
+    "revision": "4867141f190fc64b2098f0344f82f658",
+    "url": "/pagina-lyntech-corp/static/media/mision.4867141f.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/pagina-lyntech-corp/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/pagina-lyntech-corp/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/pagina-lyntech-corp/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/pagina-lyntech-corp/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a510da4d3f66e8829c20a3f73a5c7fa5",
+    "url": "/pagina-lyntech-corp/static/media/vision.a510da4d.svg"
+  }
+]);
